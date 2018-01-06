9c3c3f643a451edd2a6cd153207226c595d272f5
Lucas Andrade Soares <lucas.systema@gmail.com>
2018-01-06 16:19:39 -0200

    Configuração inicial do cors

--- config/manhattan_connection.js
+++ config/manhattan_connection.js
@@ -1,6 +1,6 @@
 module.exports = {
   user: 'ndadmin',
-  host: '192.168.1.8',
+  host: '192.168.0.103',
   database: 'datand',
   password: '123',
   port: 5432
--- config/middlewares.js
+++ config/middlewares.js
@@ -1,8 +1,16 @@
 import bodyParser from "body-parser";
+import cors from "cors";
+import express from "express";
 
 module.exports = (app) => {
   // Fazer: Pesquisar como verificar se uma porta está em uso
   app.set("THE_DOOR", 3000);
+  app.use(cors({
+    origin: ["http://google.com.br"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+  }));
   app.set("json spaces", 2);
   app.use(bodyParser.json());
+  app.use(express.static("public"));
 };
